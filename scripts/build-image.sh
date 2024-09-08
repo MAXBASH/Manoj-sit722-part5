c41b6c4f28ab6bef47542081c0450e37979153dc
Manoj-S223817296 <s223817296@deakin.edu.au>
2024-09-08 22:33:09 +1000

    Updated build image.sh

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -14,5 +14,5 @@
 set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
 
-docker buildx build --platform linux/amd64 -t $CONTAINER_REGISTRY/book_catalog:latest --file ../book_catalog/Dockerfile .
-docker buildx build --platform linux/amd64 -t $CONTAINER_REGISTRY/inventory_management:latest --file ../inventory_management/Dockerfile .
+docker buildx build --platform linux/amd64 -t $CONTAINER_REGISTRY/book_catalog:latest --file ./book_catalog/Dockerfile .
+docker buildx build --platform linux/amd64 -t $CONTAINER_REGISTRY/inventory_management:latest --file ./inventory_management/Dockerfile .
