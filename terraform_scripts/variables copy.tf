c064e056ee7ee5a0f184cbffdcb6c888d906c865
Manoj-S223817296 <s223817296@deakin.edu.au>
2024-09-22 01:15:34 +1000

    Updated code

---
+++ terraform_scripts/variables copy.tf
@@ -0,0 +1,13 @@
+# Sets global variables for this Terraform project.
+
+variable app_name {
+  default = "sit722projpart5"
+}
+
+variable location {
+  default = "australiasoutheast"
+}
+
+variable kubernetes_version {   
+  default = "1.31.0" 
+}
