3556a23d82653696384802e78fb0c2cc9f520bf5
Manoj-S223817296 <s223817296@deakin.edu.au>
2024-09-22 01:40:09 +1000

    Updated code

--- terraform_scripts/variables.tf
+++ terraform_scripts/variables.tf
@@ -2,12 +2,15 @@
 
 variable app_name {
   default = "sit722projpart5"
+  type = string
 }
 
 variable location {
   default = "australiasoutheast"
+  type = string
 }
 
 variable kubernetes_version {   
-  default = "1.31.0" 
+  default = "1.31.0"
+  type = string
 }
