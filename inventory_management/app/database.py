37e6d0115c772c21e2633ddf3a9176f8f8d86317
Manoj-S223817296 <s223817296@deakin.edu.au>
2024-09-22 02:59:15 +1000

    Updated code in book_catalog and inventory_management

--- inventory_management/app/database.py
+++ inventory_management/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://projectpart5_user:Dg0puVQqzqrs4kGaOY6jcl5pTLjxKoJV@dpg-creodg5ds78s73cf5l2g-a.oregon-postgres.render.com/projectpart5"
+SQLALCHEMY_DATABASE_URL = "postgresql://test_m48p_user:l9DCnsKLYJ60gyq1cbFAwqLQLFUGq5sS@dpg-crnfg688fa8c738fbcq0-a.oregon-postgres.render.com/test_m48p"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
