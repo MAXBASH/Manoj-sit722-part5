2337b0c66e52fbf9fa97f5263d2da03e9444a2d5
MAXBASH <64033887+MAXBASH@users.noreply.github.com>
2024-10-01 02:51:39 +1000

    Update push-image.sh

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -15,8 +15,7 @@
 set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
 : "$REGISTRY_UN"
-: "$REGISTRY_PW"
 
-echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
+echo $(az acr credential show --name $REGISTRY_UN --query "passwords[0].value" --output tsv) | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
 docker push $CONTAINER_REGISTRY/book_catalog:latest
 docker push $CONTAINER_REGISTRY/inventory_management:latest
